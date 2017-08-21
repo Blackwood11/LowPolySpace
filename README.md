03f090b066f33cf3b0c3ac8505adc118da4904bf
Blackwood11 <30394952+Blackwood11@users.noreply.github.com>
2017-08-21 21:35:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# a35a6281hba9862v86aas87bdt98astdn9a8st6d96a8snd9a" 
---
+++ Source/LowPolySpace/BlankPLayerController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "BlankPLayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LOWPOLYSPACE_API ABlankPLayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
